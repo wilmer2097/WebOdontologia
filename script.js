d8cf254e98225196f51e897cb2cfecf2763f377e
wilmer2097 <77030589+wilmer2097@users.noreply.github.com>
2023-05-25 18:12:29 -0500

    Avance

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function animateValue(element, start, end, duration) {
-    let current = start;
-    const increment = Math.floor((end - start) / duration);
-    const obj = document.querySelector(element);
-    const timer = setInterval(() => {
-      current += increment;
-      obj.innerHTML = current;
-      if (current >= end) {
-        clearInterval(timer);
-        obj.innerHTML = end;
-      }
-    }, 10);
-  }
-  
-  const counters = document.querySelectorAll('.counter');
-  counters.forEach(counter => {
-    const target = parseInt(counter.textContent);
-    animateValue(counter, 200, target, 4000);
-  });
-  
+const counters = document.querySelectorAll('.value');
+const speed = 200;
+
+counters.forEach(counter => {
+    const animate = () => {
+        const value = +counter.getAttribute('akhi');
+        const data = +counter.innerText;
+
+        const time = value / speed;
+        if (data < value) {
+            counter.innerText = Math.ceil(data + time);
+            setTimeout(animate, 1);
+        } else {
+            counter.innerHtml = `<h2>` + value + `</h2>`;
+        }
+
+    }
+
+    animate();
+});
