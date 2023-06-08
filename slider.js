db66bd210a084cb91bf2d86fe782c230c1b3eb1c
Luen08 <110075878+Luen08@users.noreply.github.com>
2023-06-08 01:13:37 -0500

    sliders

--- slider.js
+++ slider.js
@@ -5,3 +5,43 @@ $("#slider").on("input change", (e)=>{
     // Update the position of the slider button
     $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`)
   });
+
+  $("#slider1").on("input change", (e)=>{
+    const sliderPos = e.target.value;
+    // Update the width of the foreground image
+    $('.foreground-img1').css('width', `${sliderPos}%`)
+    // Update the position of the slider button
+    $('.slider-button1').css('left', `calc(${sliderPos}% - 18px)`)
+  });
+
+  $("#slider2").on("input change", (e)=>{
+    const sliderPos = e.target.value;
+    // Update the width of the foreground image
+    $('.foreground-img2').css('width', `${sliderPos}%`)
+    // Update the position of the slider button
+    $('.slider-button2').css('left', `calc(${sliderPos}% - 18px)`)
+  });
+
+  $("#slider3").on("input change", (e)=>{
+    const sliderPos = e.target.value;
+    // Update the width of the foreground image
+    $('.foreground-img3').css('width', `${sliderPos}%`)
+    // Update the position of the slider button
+    $('.slider-button3').css('left', `calc(${sliderPos}% - 18px)`)
+  });
+
+  $("#slider4").on("input change", (e)=>{
+    const sliderPos = e.target.value;
+    // Update the width of the foreground image
+    $('.foreground-img4').css('width', `${sliderPos}%`)
+    // Update the position of the slider button
+    $('.slider-button4').css('left', `calc(${sliderPos}% - 18px)`)
+  });
+
+  $("#slider5").on("input change", (e)=>{
+    const sliderPos = e.target.value;
+    // Update the width of the foreground image
+    $('.foreground-img5').css('width', `${sliderPos}%`)
+    // Update the position of the slider button
+    $('.slider-button5').css('left', `calc(${sliderPos}% - 18px)`)
+  });
