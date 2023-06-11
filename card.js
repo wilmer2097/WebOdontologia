81d8177fa64fed53d80ef12f38cec9b859afb80c
Fernando Paco <fernandopacof@gmail.com>
2023-06-11 16:35:48 -0500

    cards

--- card.js
+++ card.js
@@ -1,16 +1,21 @@
-var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 300,
-        modifier: 1,
-        slideShadows: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-});
+
+$(".carousel").owlCarousel({
+    loop: true,
+    margin: 25,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    nav: true,
+    navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 3,
+        },
+    }
+})
